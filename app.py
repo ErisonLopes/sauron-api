8d0c45631861b3b17583b87f4dd562318984aa4f
Erison Lopes <erilopesh@hotmail.com>
2019-10-03 01:03:18 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Api
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+if __name__ == '__main__':
+    app.run(debug=True)
