bce729a4df0af186a306c1a126968356afcf6b47
Erison Lopes <erilopesh@hotmail.com>
2019-10-03 01:05:51 -0300

    Criar arquivo de dependencias

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crud.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'sauron.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if __name__ == '__main__':
